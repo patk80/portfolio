1d521da4fe6c46eb0cb62cfd7b01391858cef10f
Pat Kelly iMedia <pkelly@imediainc.com>
2022-05-12 10:33:58 -0400

    Close navlinks on click JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
+let navLinks         = document.querySelectorAll(".nav-link");
+let navbarCollapse   = document.querySelector(".navbar-collapse");
 const copyrightDate = document.querySelector("#footer-copyright-date");
+
+// Looping through all navLinks and adding an event listener to close navbar when clicked (made for mobile) //
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        navbarCollapse.classList.remove("show");
+    });
+});
 
 // Automatically Update Footer Date with Current Year //
 copyrightDate.innerText = new Date().getFullYear();
