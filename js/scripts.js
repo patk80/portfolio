830431f2b485a3c76e204719d0dca828a902f55d
Pat Kelly <PatK80@gmail.com>
2022-05-12 22:24:01 -0400

    Hamburger icon working + on navlink clicks

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
 let navLinks         = document.querySelectorAll(".nav-link");
 let navbarCollapse   = document.querySelector(".navbar-collapse");
 const copyrightDate = document.querySelector("#footer-copyright-date");
+const hamburgerIcon = document.querySelectorAll(".hamburger-icon");
 
-// Looping through all navLinks and adding an event listener to close navbar when clicked (made for mobile) //
-navLinks.forEach(link => {
-    link.addEventListener("click", () => {
-        navbarCollapse.classList.remove("show");
+// Hamburger icon listening for click to toggle + Looping through all navLinks and adding an event listener to close navbar when clicked (made for mobile) //
+hamburgerIcon.forEach (icon => {  
+  icon.addEventListener("click", (event) => {
+    icon.classList.toggle("open");
+    navLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            icon.classList.remove("open");
+            navbarCollapse.classList.remove("show");
+        });
     });
+  });
 });
 
 // Automatically Update Footer Date with Current Year //
